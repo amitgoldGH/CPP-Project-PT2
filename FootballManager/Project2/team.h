7dde210d32450f285300a8825b881dda6e4f286a
Amit Goldshtein <Amit.Goldshtein@s.afeka.ac.il>
2019-10-05 18:42:16 +0300

    Changed the operator+ method to += (confirmed with Riki it's meant to be like that)
    Changed benchplayers and lineup from pointer to pointer array (to contain multiple pointers).
    
    Added include for coach and manager header files to work on addCoach and addManager functions.

--- FootballManager/Project2/team.h
+++ FootballManager/Project2/team.h
@@ -1,13 +1,15 @@
 #ifndef __TEAM_H
 #define __TEAM_H
 #include "person.h" // For NAME_SIZE definition.
+#include "coach.h"
+#include "manager.h"
 class Coach;
 class Player;
 class Manager;
 
 const int LINEUP_SIZE = 5;// size of the team lineup 
 static const char* Roles[15] = { "Striker", "Defender", "Goal-Keeper", "Middle-Fielder" };
-// Added array for each goal for toString purposes -Amit
+// Added array for each role for toString purposes -Amit
 
 class Team 
 {
@@ -22,10 +24,10 @@ public:
 		int points=0);
 	~Team();
 
-	void setManager(Manager * manger);
-	void addCoach(Coach * coach);
-	void addPlayer(Player * player);
-	Team operator+(int points) const;
+	void setManager(Manager* manger);
+	void addCoach(Coach* coach);
+	void addPlayer(Player* player);
+	const Team& operator+=(int points); // Changed from operator+ confirmed with Riki it was a mistake.
 	bool operator >=(const Team& otherTeam) const; //Team is bigger if team have more pointprivate:
 	const char* getName() const; // Added method for Player to display team name, as team name is private -Amit
 
@@ -33,8 +35,9 @@ private:
 	char name[NAME_SIZE];
 	Manager * manager;
 	Coach * coaches;
-	Player * benchPlayers;
-	Player * lineup;
+	Player** benchPlayers; // Changed to Pointer array from single pointer
+	Player** lineup; // Changed to Pointer array from single pointer,
+					 //to allow multiple player pointers in the array
 	int points;
 };
 
