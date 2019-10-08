9937e8332867160dbef39f91eb7264a20b6abb5b
Amit Goldshtein <Amit.Goldshtein@s.afeka.ac.il>
2019-10-08 18:57:12 +0300

    Defined show method to display the team's name, manager's, coach's, lineup and bench information.
    Changed lineup and bench to be dynamically allocated incase a nullptr is given in constructor, and then assigned nullptr to each slot in the newly created arrays.
    
    if a coach/manager are given instead of nullptr, sets their team as the team being created in this constructor.
    
    Changed the destructor to delete benchplayers and lineup as they're now dynamically allocated.
    
    defined the "addPlayer" function which adds players to lineup if it's not full, otherwise to bench (if its not full).

--- FootballManager/Project2/team.cpp
+++ FootballManager/Project2/team.cpp
@@ -4,6 +4,29 @@ const char* Team::getName() const
 	return name;
 }
 
+void Team::show() const
+{
+	std::cout << "Team Name: " << name 
+		<< "\nTeam Manager: \n"; manager->show();
+	std::cout << "\nTeam Coach: \n"; coaches->show();
+	
+	std::cout << "\nLineup: \n" << std::endl;
+	for (int i = 0; i < LINEUP_MAX_SIZE; ++i)
+		if (this->lineup[i] != nullptr)
+		{
+			this->lineup[i]->show();
+			std::cout << std::endl;
+		}
+	
+	std::cout << "\nBench bois: " << std::endl;
+	for (int i = 0; i < LINEUP_MAX_SIZE * BENCH_SIZE_MULTI; ++i)
+		if (benchPlayers[i] != nullptr)
+		{
+			benchPlayers[i]->show();
+			std::cout << std::endl;
+		}
+}
+
 Team::Team(const char name[NAME_SIZE], Manager* manager, Coach* coaches,
 	Player** lineup, Player** benchPlayers, int lineup_Size, int bench_Size, int points) 
 	: manager(manager), coaches(coaches), lineup(lineup), benchPlayers(benchPlayers),
@@ -12,19 +35,31 @@ Team::Team(const char name[NAME_SIZE], Manager* manager, Coach* coaches,
 	strncpy_s(Team::name, name, NAME_SIZE); // Copy NAME_SIZE chars from input into name field.
 	Team::name[NAME_SIZE - 1] = '\0'; // In case input was larger than NAME_SIZE adding a null terminator to prevent overflow.
 	
+	if (coaches != nullptr)
+		coaches->setTeam(this);
+	if (manager != nullptr)
+		manager->setTeam(this);
+
 	if (Team::lineup == nullptr) // If a lineup wasn't given, dynamically allocate one of size LINEUP_SIZE
-		lineup[LINEUP_MAX_SIZE];
+	{
+		Team::lineup = new Player*[LINEUP_MAX_SIZE];
+		for (int i = 0; i < LINEUP_MAX_SIZE; ++i)
+			Team::lineup[i] = nullptr;
+	}
 	if (Team::benchPlayers == nullptr)  // If a benchPlayer array wasn't given, allocate one LINEUP_SIZE times 3.
-		benchPlayers[LINEUP_MAX_SIZE * BENCH_SIZE_MULTI];
+	{
+		Team::benchPlayers = new Player*[LINEUP_MAX_SIZE * BENCH_SIZE_MULTI];
+		for (int i = 0; i < LINEUP_MAX_SIZE * BENCH_SIZE_MULTI; ++i)
+			Team::benchPlayers[i] = nullptr;
+	}
 }
 
 Team::~Team() 
 {
-	delete[] name;
-	/*
+	// delete[] name; not dynamically assigned -Amit
 	delete[] benchPlayers;
 	delete[] lineup;
-	*/
+
 }
 
 void Team::setManager(Manager* manager)
@@ -49,6 +84,32 @@ void Team::addCoach(Coach* coach)
 
 void Team::addPlayer(Player* player)
 {
+	if (player != nullptr) // Player actually exists
+		if (player->getTeam() != this)
+		{
+			if (players_In_Lineup < LINEUP_MAX_SIZE) // Lineup isn't full.
+			{
+				for (int i = 0; i < LINEUP_MAX_SIZE; ++i)
+					if (lineup[i] == nullptr)
+					{
+						lineup[i] = player;
+						lineup[i]->setTeam(this);
+						++players_In_Lineup;
+						break;
+					}
+			}
+			else if (players_On_Bench < LINEUP_MAX_SIZE * BENCH_SIZE_MULTI) // Bench isn't full
+			{
+				for (int i = 0; i < LINEUP_MAX_SIZE * BENCH_SIZE_MULTI; ++i)
+					if (benchPlayers[i] == nullptr)
+					{
+						benchPlayers[i] = player;
+						benchPlayers[i]->setTeam(this);
+						++players_On_Bench;
+						break;
+					}
+			}
+		}
 
 }
 
