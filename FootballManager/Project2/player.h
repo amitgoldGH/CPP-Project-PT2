a9e2ede727f09a63ebafcbcf5b0fb5aca915b52b
Amit Goldshtein <Amit.Goldshtein@s.afeka.ac.il>
2019-10-08 17:06:49 +0300

    Moved the enum role type and char array from Team to Player to solve include loop error.
    Also role is related to the player, as there are player types, not to Team ,as there are no team types.

--- FootballManager/Project2/player.h
+++ FootballManager/Project2/player.h
@@ -3,24 +3,26 @@
 #include "person.h"
 #include "team.h"
 
+static const char* Roles[15] = { "Striker", "Defender", "Goal-Keeper", "Middle-Fielder" };
+
 class Player : virtual public Person
 {
 
 public:
-	
+	enum Role { striker, defender, goalkeeper, midlefielder };
 	void show() const; // Changed to virtual for inheritors to override.
 	double getRate() const; // Changed to const function -Amit
 
 	Player(const char* name, int age,
 		const char* nationality,
 		int attack, int defence,
-		int goalkeeping, int goalScored, int value=0,
-		Team::Role role= (Team::Role)0, Team * currentTeam = nullptr);
+		int goalkeeping = 0, int goalScored = 0, int value=0,
+		Role role = (Role)0, Team* currentTeam = nullptr);
 	void setTeam(Team* team);
 	Player operator++(int); // This is a postfix operator. -Amit
 	const Player& operator++(); // Added prefix operator -Amit
 	bool operator >=(const Player& player) const; //Player is bigger if he scored more goles #Goals*
-
+	const Team* getTeam(); // Added to check what team a player is in -Amit
 	~Player();
 
 protected:
@@ -29,7 +31,7 @@ protected:
 	int goalkeeping;
 	int goalScored;
 	int value;
-	Team::Role role;
+	Role role;
 	Team * currentTeam;
 
 	Player(const Player& other);
