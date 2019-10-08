ac0be0cd914aad096eefb04b8b57381096d95ba0
Amit Goldshtein <Amit.Goldshtein@s.afeka.ac.il>
2019-10-08 17:10:02 +0300

    Adjusted constructor definition for new phys size variables.
    Also added an if statement for each case where the lineup and bench arrays might not be given.

--- FootballManager/Project2/team.cpp
+++ FootballManager/Project2/team.cpp
@@ -1,22 +1,30 @@
 #include "team.h"
-
 const char* Team::getName() const
 {
 	return name;
 }
 
 Team::Team(const char name[NAME_SIZE], Manager* manager, Coach* coaches,
-	Player* lineup, Player* benchPlayers, int points) 
-	: manager(manager), coaches(coaches), lineup(lineup),
-	benchPlayers(benchPlayers), points(points)
+	Player** lineup, Player** benchPlayers, int lineup_Size, int bench_Size, int points) 
+	: manager(manager), coaches(coaches), lineup(lineup), benchPlayers(benchPlayers),
+	players_In_Lineup(lineup_Size), players_On_Bench(bench_Size), points(points)
 {
 	strncpy_s(Team::name, name, NAME_SIZE); // Copy NAME_SIZE chars from input into name field.
 	Team::name[NAME_SIZE - 1] = '\0'; // In case input was larger than NAME_SIZE adding a null terminator to prevent overflow.
+	
+	if (Team::lineup == nullptr) // If a lineup wasn't given, dynamically allocate one of size LINEUP_SIZE
+		lineup[LINEUP_MAX_SIZE];
+	if (Team::benchPlayers == nullptr)  // If a benchPlayer array wasn't given, allocate one LINEUP_SIZE times 3.
+		benchPlayers[LINEUP_MAX_SIZE * BENCH_SIZE_MULTI];
 }
 
 Team::~Team() 
 {
 	delete[] name;
+	/*
+	delete[] benchPlayers;
+	delete[] lineup;
+	*/
 }
 
 void Team::setManager(Manager* manager)
