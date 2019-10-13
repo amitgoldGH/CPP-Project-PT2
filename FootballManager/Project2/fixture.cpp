dedaa3034e90b6cb2e347bcfbb9b5dc5ef9b9b18
Amit Goldshtein <Amit.Goldshtein@s.afeka.ac.il>
2019-10-13 14:59:56 +0300

    Added destructor and << operator

---
+++ FootballManager/Project2/fixture.cpp
@@ -0,0 +1,15 @@
+#include "fixture.h"
+
+Fixture::Fixture(int number, Match* matches) : number(number) , matches(matches) {}
+
+Fixture::~Fixture()
+{
+	delete[] matches;
+}
+
+Fixture::Fixture(const Fixture& other) : number(other.number), matches(other.matches) {}
+
+std::ostream& operator<<(std::ostream& os, const Fixture& fixture)
+{
+	os << fixture.number << " " << fixture.matches;
+}
