1b785551215c297ed002bcdaa2f80bef50151fb0
Amit Goldshtein <Amit.Goldshtein@s.afeka.ac.il>
2019-10-25 10:22:48 +0300

    Changed addMatch to work with vector instead of old Match* array
    Changed destructor as no longer required to release dynamically allocated.
    
    Changed show to use for each loop with vector.
    
    Changed << operator as it's not working anyway

--- FootballManager/Project2/fixture.cpp
+++ FootballManager/Project2/fixture.cpp
@@ -1,49 +1,34 @@
 #include "fixture.h"
 
-Fixture::Fixture(int number, int numberOfMatches, Match* matches) : number(number), matches(matches), numberOfMatches(numberOfMatches)
+Fixture::Fixture(int number, std::vector<Match> matches) : number(number), matches(matches)
 {
 
 }
 
 
-Fixture::~Fixture()
-{
-	 delete[] matches;
-}
+Fixture::~Fixture(){}
 
 Fixture::Fixture(const Fixture& other) : number(other.number), matches(other.matches) {}
 
 void Fixture::addMatch(Match * m)
 {
-	Match* increased_Matches = new Match[numberOfMatches + 1];
-	Match* temp;
-	for (int i = 0; i < numberOfMatches; i++)
-	{
-		increased_Matches[i] = matches[i];
-	}
-	increased_Matches[numberOfMatches] = (Match(*m));
-	
-	temp = matches;
-	matches = increased_Matches;
-
-	numberOfMatches++;
-	delete[] temp;
+	matches.push_back(*m);
 }
 
 void Fixture::Show()
 {
 
 	std::cout << "Fixture " << number << ":" << std::endl;
-	for (int i = 0; i < numberOfMatches; i++)
-	{
-		matches[i].show();
-	}
+	for (Match match : matches)
+		match.show();
 }
 
 
 
-std::ostream& operator<<(std::ostream& os, const Fixture& fixture)
+std::ostream& operator<<(std::ostream& os, const Fixture& fixture) // NOT WORKING, MATCH DOESNT HAVE << operator
 {
-	os << fixture.number << " " << fixture.matches;
+	os << fixture.number << " ";
+	for (Match m : fixture.matches)
+		os << &m;
 	return os;
 }
